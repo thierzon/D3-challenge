a599655c712580ad979009ca8809528e8e044fb9
thierzon <thierryzonneveld@gmail.com>
2021-04-24 17:31:53 +0800

    Completed core assignment

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,113 @@
-// @TODO: YOUR CODE HERE!
+var svgWidth = 1075;
+var svgHeight = 750;
+
+var margin = {
+  top: 20,
+  right: 20,
+  bottom: 80,
+  left: 60
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Initial Params
+var chosenXAxis = "poverty";
+
+// function used for updating x-scale var upon click on axis label
+function xScale(stateData, chosenXAxis) {
+  // create scales
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(stateData, d => d[chosenXAxis]) * 0.9, d3.max(stateData, d => d[chosenXAxis]) * 1.1])
+    .range([0, width]);
+
+  return xLinearScale;
+}
+
+// Retrieve data from the CSV file and execute everything below
+d3.csv("assets/data/data.csv").then(function(stateData, err) {
+  if (err) throw err;
+
+  // parse data
+  stateData.forEach(function(data) {
+    data.abbr = data.abbr;
+    data.poverty = +data.poverty;
+    data.healthcare = +data.healthcare;
+  });
+
+  // xLinearScale function above csv import
+  var xLinearScale = xScale(stateData, chosenXAxis);
+
+  // Create y scale function
+  var yLinearScale = d3.scaleLinear()
+    .domain([d3.min(stateData, d => d.healthcare) * 0.9, d3.max(stateData, d => d.healthcare) * 1.1])
+    .range([height, 0]);
+
+  // Create initial axis functions
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // append x axis
+  var xAxis = chartGroup.append("g")
+    .classed("x-axis", true)
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  // append y axis
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  // append circles
+  var circlesGroup = chartGroup.selectAll()
+    .data(stateData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d[chosenXAxis]))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", 15)
+    .classed("stateCircle", true);
+
+  var textGroup = chartGroup.selectAll()
+    .data(stateData)
+    .enter()
+    .append("text")
+    .attr("transform", `translate(0, 5)`)
+    .text(d => (d.abbr))
+    .attr("x", d => xLinearScale(d[chosenXAxis]))
+    .attr("y", d => yLinearScale(d.healthcare))
+    .classed("stateText", true);
+
+  // Create group for x-axis label
+  var labelsGroup = chartGroup.append("g")
+    .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+  var ageLabel = labelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 40)
+    .attr("poverty", "healthcare")
+    .text("In Poverty (%)");
+
+  // append y axis
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .text("Lacks Healthcare (%)");
+
+}).catch(function(error) {
+  console.log(error);
+});
+
